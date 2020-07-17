1e02e8cc886009bb835cb66ec8b4bdc9e5ba03b1
Michael Truss <badmoonrising31@gmail.com>
2020-07-16 23:44:03 -0500

    Add files via upload

---
+++ build/precache-manifest.1759a26d20f92e1257702c64ec2d9504.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfdae1f48e121b2e9c2df9468b18edb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d8c9e52704d1bab29c3",
+    "url": "/static/css/main.6b73d613.chunk.css"
+  },
+  {
+    "revision": "d661b0d815234a5401ea",
+    "url": "/static/js/2.44998ee1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.44998ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d8c9e52704d1bab29c3",
+    "url": "/static/js/main.80dab9de.chunk.js"
+  },
+  {
+    "revision": "3b906a68717433f63125",
+    "url": "/static/js/runtime-main.33336675.js"
+  },
+  {
+    "revision": "0c2a19122a407d9229f5f63ed034ea3b",
+    "url": "/static/media/image1.0c2a1912.jpeg"
+  },
+  {
+    "revision": "9d884eb0b511242f7eeab3c9d6db0d49",
+    "url": "/static/media/image2.9d884eb0.jpeg"
+  }
+]);
